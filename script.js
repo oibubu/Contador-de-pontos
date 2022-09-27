3d6cf1ae0729b92f6cf63c09322cd979de849d11
Bubuzito <vilhenabruno0@gmail.com>
2022-09-26 21:24:15 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var bruno = { nome: "Bruno", vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
-var pedro = { nome: "Pedro", vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
-var mazza = { nome: "Mazza", vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
-var hugo = { nome: "Hugo", vitorias: 0, derrotas: 0, empates: 0, pontos: 0}
-var pontosWin = ''
-var jogadores = [bruno, pedro, mazza, hugo]
