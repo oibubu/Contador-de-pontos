bd9fd58581cda2dbbead1fffe5d52deae9dae8a3
Bubuzito <vilhenabruno0@gmail.com>
2022-09-27 20:35:32 -0300

    Update scriptfunction.js

--- scriptfunction.js
+++ scriptfunction.js
@@ -1,6 +1,7 @@
+var validarJogadores = []
 var jogadores = []
 var pontosWin = ''
-
+var resultado = document.getElementById('resultadoJogo')
 
 function adicionarJogador(){
   var newPlayer = document.getElementById('nomeJogadores').value
@@ -8,7 +9,13 @@ function adicionarJogador(){
   if (newPlayer.length == 0){
     window.alert('Adicione pelo menos um nome!')
   }else{
-    jogadores.push({'nome' : newPlayer, 'vitorias' : 0, 'empates' : 0, 'derrotas' : 0, 'pontos' :0})
+    if (validarJogadores.indexOf(newPlayer) != -1){
+      console.log(validarJogadores)
+      window.alert('Jogador já adicionado!')
+    }else{
+      validarJogadores.push(newPlayer)
+      jogadores.push({'nome' : newPlayer, 'vitorias' : 0, 'empates' : 0, 'derrotas' : 0, 'pontos' :0})
+    }
   }
   resultado.innerHTML = ""
   for (i in jogadores){
@@ -39,11 +46,11 @@ function adicionarNaTabela(){
         elemento += "<td>" + jogadores[i].derrotas + "</td>";
         elemento += "<td>" + jogadores[i].pontos + "</td>";
         elemento +=
-          "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>";
+          "<td><button onClick='adicionarVitoria(" + i + ")' class='vitoria'>Vitória</button></td>";
         elemento +=
-          "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>";
+          "<td><button onClick='adicionarEmpate(" + i + ")' class='empate'>Empate</button></td>";
         elemento +=
-          "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td>";
+          "<td><button onClick='adicionarDerrota(" + i + ")' class='derrota'>Derrota</button></td>";
         elemento += "</tr>";
     }
     tabela.innerHTML = elemento
@@ -82,11 +89,23 @@ function adicionarDerrota(i){
 }
 
 function vencedor(x){
-  var resultado = document.getElementById('resultadoJogo')
       if (x.pontos >= pontosWin){
           resultado.innerHTML = `<h1>Parabens ${x.nome} Você venceu!!</h1>`
+          resultado.innerHTML += "<p><input type='button' value='Reiniciar' onclick=" + 'reiniciar()' + " class='classeBotao'></p>"
           tabelaJogo.hidden = true
         }
+}
+
+function reiniciar(){
+  resultado.innerHTML = ''
+  tabelaJogo.hidden = false
+  for (var i in jogadores){
+    jogadores[i].vitorias = 0
+    jogadores[i].empates = 0
+    jogadores[i].derrotas = 0
+    jogadores[i].pontos = 0
+  }
+  adicionarNaTabela(jogadores)
 }
 
 function iniciarJogo(){
